39443eca0d7a6445e31926d617c9213e9f229cb9
Michael Goldspinner <emgo.development@gmail.com>
2019-01-27 02:16:41 -0500

    chore(files): remove shell & markdown files from repo root
    Removing files from repository root.

--- .rebashrc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# .: this calls a command from an executable file
-# ~/: is the home directory of the current user
-# .bashrc: is an executable file containing terminal aliases.
-## We are going to execute this command to 'reinitialize' out collection of system wide aliases.
-## Aliases must be added to the end of the file before this command is run for changes to take effect.
-
-. ~/.bashrc
